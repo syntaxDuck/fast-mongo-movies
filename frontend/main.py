4ef15ec7503e0fa531284b59eb1ba1a707bd496e
syntaxDuck <kam.comer96@gmail.com>
2024-09-12 23:53:29 -0400

    Started creating the movie details section

--- frontend/main.py
+++ frontend/main.py
@@ -1,96 +1,146 @@
 from fasthtml.common import *
 import requests
 
-app, rt = fast_app(live=True)
+app, rt = fast_app(
+    live=True,
+    hdrs=(
+        Style(
+            """
+            .box {
+                display:flex;
+                flex-direction: row; 
+            }
 
+            .movie-details {
+                padding-top: 1em;
+                flex-grow: 1; 
+                display: flex;
+                justify-content: center;
+            }
 
-# @rt("/")
-# def get():
-#     return Div(P("Hello World!"), hx_get="/change")
+            .movie-details img { background-color: #f2f2f2;
+                    border-radius: 8px;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                    overflow: hidden;
+                    transition: transform 0.2s; }
 
+            .movies { max-width: 20%;
+                    height: 100vh;
+                    padding: 1em;
+                    overflow-y: scroll;
+                    overflow-x: hidden;
+                    box-sizing: border-box;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    display: flex; 
+                    flex-direction: column; }
 
-# @rt("/change")
-# def get():
-#     return P("Nice to be here!")
+            .movies button { margin: 20px; padding: 2px; }
+
+                     
+            .movie-list { margin: 0; padding: 0;list-style-type: none; }
+            
+            .movie { background-color: #f2f2f2;
+                    border-radius: 8px;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                    overflow: hidden;
+                    transition: transform 0.2s; }
+            """
+        ),
+    ),
+)
 
 api_url = "http://127.0.0.1:8000"
 
 
-@app.route("/", methods="get")
-def home():
-    url = f"{api_url}/movies"
-    params = {"type": "movie"}
-    data = requests.get(url, params=params)
-    data = data.json()
-    # return Img(src=data[1]["poster"], style="width: 10%")
-    stock = "https://thumbs.dreamstime.com/b/film-real-25021714.jpg"
-    movies = []
-    for movie in data:
-        if movie["poster"]:
-            movies.append(
-                Img(src=movie["poster"])
-                + P(
-                    movie["title"],
-                    style="text-align: center; color: black",
-                )
-            )
-        else:
-            movies.append(
-                Img(src=stock)
-                + P(movie["title"], style="text-align: center; color: black")
-            )
-
+def movie_list():
     return Div(
         Ul(
-            (
-                Li(
-                    (movie),
-                    style="""background-color: #f2f2f2;
-                        border-radius: 8px;
-                        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                        overflow: hidden;
-                        transition: transform 0.2s;""",
-                )
-                for movie in movies
-            ),
-            style="margin: 0; padding: 0;list-style-type: none;",
+            cls="movie-list",
+            hx_get="/movies",
+            hx_trigger="load",
+            hx_swap="innerHTML",
         ),
-        style=(
-            """max-width: 20%;
-            height: 100vh;
-            padding: 20px;
-            overflow-y: scroll;
-            overflow-x: hidden;
-            box-sizing: border-box;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);"""
+        Button(
+            "Next",
+            hx_get="/movies",
+            hx_target=".movie-list",
+            hx_swap="beforeend",
         ),
+        cls="movies",
     )
 
 
-@app.route("/card", methods="get")
-def card_3d_demo():
-    """This is a standalone isolated Python component.
-    Behavior and styling is scoped to the component."""
+def movie_details():
+    return Div(cls="movie-details")
 
-    def card_3d(text, background, amt, left_align):
-        # JS and CSS can be defined inline or in a file
-        # scr = ScriptX("card3d.js", amt=amt)
-        align = "left" if left_align else "right"
-        sty = StyleX("", background=f"url({background})", align=align)
-        return Div(text, Div(), sty, "")
 
-    # Design credit: https://codepen.io/markmiro/pen/wbqMPa
-    url = f"{api_url}/movies"
-    params = {"type": "movie"}
-    data = requests.get(url, params=params)
-    data = data.json()
-    card = card_3d("Mouseover me", data[0]["poster"], amt=1.5, left_align=True)
-    return Div(card, style=None)
+@app.route("/", methods="get")
+def home():
+    global page
+    page = 0
+    return Div(movie_list(), movie_details(), cls="box")
 
 
 @app.route("/", methods=["post", "put"])
 def post_or_put():
     return "got a POST or PUT request"
+
+
+page = 0
+movies = []
+
+
+@app.get("/details/{index}")
+def get_details(index: int):
+    return Div(Img(src=movies[index]["poster"]))
+
+
+@app.get("/movies")
+def get_movies():
+    global page
+    global movies
+
+    url = f"{api_url}/movies?limit=10&skip={page*10}"
+    params = {"type": "movie"}
+    data = requests.get(url, params=params)
+    data = data.json()
+    default_img = "https://thumbs.dreamstime.com/b/film-real-25021714.jpg"
+
+    count = 0
+    list_movies = []
+    for movie in data:
+        image = default_img
+        if movie["poster"]:
+            response = requests.head(movie["poster"], allow_redirects=False)
+            if response.status_code == 200:
+                image = movie["poster"]
+            else:
+                image = default_img
+        else:
+            image = default_img
+
+        movies.append(movie)
+
+        list_movies.append(
+            Li(
+                Img(
+                    src=image,
+                ),
+                P(
+                    movie["title"],
+                    style="text-align: center; color: black",
+                ),
+                cls="movie",
+                hx_get=f"/details/{count}",
+                hx_target=".movie-details",
+                hx_swap="innerHTML",
+                hx_trigger="click",
+            )
+        )
+        count += 1
+
+    page += 1
+    return list_movies
 
 
 serve()
